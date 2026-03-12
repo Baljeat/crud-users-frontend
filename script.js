4c170f6bef8b5d76a6cc0ceff9d7f31b48b1f6ab
Baljeat_Les <hlan9693@gmail.com>
2026-03-12 20:11:00 +0700

    remake style.css

--- script.js
+++ script.js
@@ -1,34 +1,80 @@
-const API_URL = "https://crud-user-uzzz.onrender.com/users"
+const API = "https://crud-user-uzzz.onrender.com/users"
+
+const table = document.getElementById("users")
+const loading = document.getElementById("loading")
 
 async function loadUsers(){
 
-const loading = document.getElementById("loading")
-loading.style.display = "block"
+loading.style.display="block"
 
-const res = await fetch(API_URL)
+const res = await fetch(API)
 const data = await res.json()
 
-const table = document.getElementById("users")
 table.innerHTML=""
 
-data.forEach(user => {
+data.forEach(user=>{
 
-const row = document.createElement("tr")
+const row=document.createElement("tr")
 
-row.innerHTML = `
+row.innerHTML=`
 <td>${user.id}</td>
 <td>${user.name}</td>
+<td>
+<button class="delete btn" onclick="deleteUser('${user.id}')">
+Delete
+</button>
+</td>
 `
 
 table.appendChild(row)
 
 })
 
-loading.style.display = "none"
+loading.style.display="none"
+
 }
 
 function reloadUsers(){
 loadUsers()
+}
+
+/* CREATE USER */
+
+document.getElementById("userForm")
+.addEventListener("submit",async e=>{
+
+e.preventDefault()
+
+const id=document.getElementById("id").value
+const name=document.getElementById("name").value
+
+await fetch(API,{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({id,name})
+})
+
+document.getElementById("id").value=""
+document.getElementById("name").value=""
+
+loadUsers()
+
+})
+
+/* DELETE USER */
+
+async function deleteUser(id){
+
+if(!confirm("Delete this user?")) return
+
+await fetch(`${API}/${id}`,{
+method:"DELETE"
+})
+
+loadUsers()
+
 }
 
 loadUsers()
